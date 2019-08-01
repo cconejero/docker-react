a453c4c8639303b0994f4a19417b05c3636998db
Claudio Conejero <cconejero@Claudios-MacBook-Air.local>
2019-08-01 12:47:32 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Prueba 4
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          A ver que onda...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
